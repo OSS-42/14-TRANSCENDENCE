68aedb89e40f21243cc76dcbc8539ea915439ad9
Cesar Menjivar <cemenjiv@student.42quebec.com>
2023-09-05 16:26:22 -0400

    Fix username problems with special characters

--- frontend/src/components/Profile/NameBox.tsx
+++ frontend/src/components/Profile/NameBox.tsx
@@ -17,32 +17,48 @@ export function NameBox(props) {
     setEditedName(newName);
   }
 
-  function isUserNameValid(username) {
+  function UserNameCharacters(username: string) {
     for (let i = 0; i < username.length; i++) {
       const char = username[i];
+      console.log("Printing:", username[i]);
       if (!/[a-zA-Z0-9]/.test(char)) {
         return false;
       }
     }
+    return true;
+  }
+
+  function UserNameLength(username: string) {
     if (username.length < 3 || username.length > 12) return false;
-    return true; // Valid username
+    return true;
   }
 
   async function handleBlur() {
     if (editedName !== props.user) {
-	  const Name = await isUserExist(editedName);
-	  if (Name === true)
-	  {
-		alert("Username is already used. Please choose another one!");
-		  setEditedName(props.user);
-		  return;
-	  }
-      if (editedName.trim() === "" || isUserNameValid(editedName) === false) {
+      if (editedName.trim() === "") {
+        alert("You cannot have an empty name. Please enter name");
+        setEditedName(props.user);
+        return;
+      }
+      if (UserNameCharacters(editedName) === false) {
         alert(
-          `Username cannot be empty, please enter a name respecting the following conditions:\n
-          - Between 3 to 12 characters
-          - Only letters and numbers (no spaces or special characters)`
-        )
+          "Invalid Username! You username must ONLY contain letters or numbers. No special characters are authorized "
+        );
+        setEditedName(props.user);
+        return;
+      }
+      if (UserNameLength(editedName) === false) {
+        alert(
+          "Invalid Username! Username must contained between 3 to 12 characters."
+        );
+        setEditedName(props.user);
+        return;
+      }
+      const Name = await isUserExist(editedName);
+      if (Name === true) {
+		alert(
+			"Invalid Username! Another user already has this username"
+		  );
         setEditedName(props.user);
         return;
       }
