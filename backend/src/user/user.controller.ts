1fd83cec49ceafb1b8527eb18f08440ded656697
Cesar Menjivar <cemenjiv@student.42quebec.com>
2023-09-05 15:20:38 -0400

    Re-added updateUser function in controller and services(backend)

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,9 +1,130 @@
-import { Body, Controller, Get, Param, Post, Req, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiBearerAuth, ApiBody, ApiHeader, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
-import { Utilisateur } from '@prisma/client';
-import { GetUser } from 'src/auth/decorator';
-import { JwtGuard } from 'src/auth/guard';
+// import { Body, Controller, Get, Param, Post, Req, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+// import { FileInterceptor } from '@nestjs/platform-express';
+// import { ApiBearerAuth, ApiBody, ApiHeader, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+// import { Utilisateur } from '@prisma/client';
+// import { GetUser } from 'src/auth/decorator';
+// import { JwtGuard } from 'src/auth/guard';
+// import { UserService } from "./user.service";
+// import * as fs from "fs";
+
+// @Controller("users")
+// @ApiTags("users")
+// @ApiBearerAuth()
+// @UseGuards(JwtGuard)
+// export class UserController {
+//   constructor(private userService: UserService) {}
+
+//   //retourne le data de l'utilisateur
+//   @Get("me")
+//   getMe(@GetUser() user: Utilisateur) {
+//     return user;
+//   }
+
+//   //retourne un array d'objets utilisateurs
+//   @Get("allUsers")
+//   getAllUsers() {
+//     return this.userService.getAllUsers();
+//   }
+//   @Get("friendsList")
+//   getFriendsList(@GetUser() user: Utilisateur) {
+//     return this.userService.getFriendsList(user);
+//   }
+
+//   //retourne le data d'un utilisateur particulier
+//   @ApiParam({ name: "username", type: String })
+//   @Get(":username")
+//   getUserInfo(@Param("username") username: string) {
+//     return this.userService.getUserInfo(username);
+//   }
+
+//   @ApiParam({ name: "id", type: Number })
+//   @Get("plus/:id")
+//   getUserInfoPlus(@Param("username") id: number) {
+//     return this.userService.getUserInfoPlus(id);
+//   }
+
+//   //ajout d<une relation d'amitié
+//   @ApiParam({ name: "username", type: String })
+//   @Get("addFriend/:username")
+//   addFriend(@Param("username") username: string, @GetUser() user: Utilisateur) {
+//     return this.userService.addFriend(user, username);
+//   }
+
+//     @ApiParam({ name: 'id', type: Number })
+//     @Get('plus/:id')
+//     getUserInfoPlus(@Param('username') id:number){
+
+//         return this.userService.getUserInfoPlus(id);
+//     }
+
+//     @ApiParam({ name: 'username', type: String })
+//     @Get('addFriend/:username')
+//     addFriend(@Param('username') username:string, @GetUser() user: Utilisateur){
+
+//         return this.userService.addFriend(user, username);
+//     }
+
+//     @Post('updateAvatar')
+//     @UseInterceptors(FileInterceptor('avatar'))
+//     updateAvatar(@GetUser() user: Utilisateur, @UploadedFile() avatarFile: any){
+
+//     const originalname = avatarFile.originalname;
+//     const mimetype = avatarFile.mimetype;
+//     const buffer = avatarFile.buffer;
+
+//     let imagePath = `./uploads/${originalname}`;
+//     fs.writeFileSync(imagePath, buffer);
+//     imagePath = imagePath.substring(1);
+//     const Url = "http://localhost:3001" + imagePath;
+//     console.log(imagePath);
+//     return this.userService.updateAvatar(user, Url);
+//   }
+
+//         return this.userService.getUserMatchHistory(id);
+//     }
+//     @ApiParam({ name: 'id', type: Number })
+//     @Get('blockedUsers/:id')
+//     getBlockedUsers(@Param('id') id:number){
+
+//         return this.userService. blockedUserIds(id);
+//     }
+//     @ApiParam({ name: 'friendId', type: Number })
+//     @Get('removeFriend/:friendId')
+//     revomeFriend(@Param('friendId') friendId:number, @GetUser() user: Utilisateur){
+
+//         return this.userService.destroyFriend(user.id, friendId);
+//     }
+//     @ApiParam({ name: 'username', type: String })
+//     @Get('userExist/:username')
+//     checkIfUserExist(@Param('username') username:string){
+
+//         return this.userService.checkIfUserExist(username);
+//     }
+// }
+
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiHeader,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from "@nestjs/swagger";
+import { Utilisateur } from "@prisma/client";
+import { GetUser } from "src/auth/decorator";
+import { JwtGuard } from "src/auth/guard";
 import { UserService } from "./user.service";
 import * as fs from "fs";
 
@@ -43,31 +164,15 @@ export class UserController {
     return this.userService.getUserInfoPlus(id);
   }
 
-  //ajout d<une relation d'amitié
   @ApiParam({ name: "username", type: String })
   @Get("addFriend/:username")
   addFriend(@Param("username") username: string, @GetUser() user: Utilisateur) {
     return this.userService.addFriend(user, username);
   }
 
-    @ApiParam({ name: 'id', type: Number })
-    @Get('plus/:id')
-    getUserInfoPlus(@Param('username') id:number){
-
-        return this.userService.getUserInfoPlus(id);
-    }
-
-    @ApiParam({ name: 'username', type: String })
-    @Get('addFriend/:username')
-    addFriend(@Param('username') username:string, @GetUser() user: Utilisateur){
-
-        return this.userService.addFriend(user, username);
-    }
-
-    @Post('updateAvatar')
-    @UseInterceptors(FileInterceptor('avatar')) 
-    updateAvatar(@GetUser() user: Utilisateur, @UploadedFile() avatarFile: any){
-        
+  @Post("updateAvatar")
+  @UseInterceptors(FileInterceptor("avatar"))
+  updateAvatar(@GetUser() user: Utilisateur, @UploadedFile() avatarFile: any) {
     const originalname = avatarFile.originalname;
     const mimetype = avatarFile.mimetype;
     const buffer = avatarFile.buffer;
@@ -80,24 +185,35 @@ export class UserController {
     return this.userService.updateAvatar(user, Url);
   }
 
-        return this.userService.getUserMatchHistory(id);
-    }
-    @ApiParam({ name: 'id', type: Number })
-    @Get('blockedUsers/:id')
-    getBlockedUsers(@Param('id') id:number){
-
-        return this.userService. blockedUserIds(id);
-    }
-    @ApiParam({ name: 'friendId', type: Number })
-    @Get('removeFriend/:friendId')
-    revomeFriend(@Param('friendId') friendId:number, @GetUser() user: Utilisateur){
-
-        return this.userService.destroyFriend(user.id, friendId);
-    }
-    @ApiParam({ name: 'username', type: String })
-    @Get('userExist/:username')
-    checkIfUserExist(@Param('username') username:string){
-
-        return this.userService.checkIfUserExist(username);
-    }
+  @ApiParam({ name: "id", type: Number })
+  @Get("matchHistory/:id")
+  getUserMatchHistory(@Param("id") id: number) {
+    return this.userService.getUserMatchHistory(id);
+  }
+  @ApiParam({ name: "id", type: Number })
+  @Get("blockedUsers/:id")
+  getBlockedUsers(@Param("id") id: number) {
+    return this.userService.blockedUserIds(id);
+  }
+  @ApiParam({ name: "friendId", type: Number })
+  @Get("removeFriend/:friendId")
+  revomeFriend(
+    @Param("friendId") friendId: number,
+    @GetUser() user: Utilisateur
+  ) {
+    return this.userService.destroyFriend(user.id, friendId);
+  }
+  @ApiParam({ name: "username", type: String })
+  @Get("userExist/:username")
+  checkIfUserExist(@Param("username") username: string) {
+    return this.userService.checkIfUserExist(username);
+  }
+  @Post("updateUsername")
+  updateUsername(
+    @GetUser() user: Utilisateur,
+    @Body() updateData: { newUsername: string }
+  ) {
+    console.log("New Username:", updateData);
+    return this.userService.updateUsername(user, updateData.newUsername);
+  }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,3 +1,253 @@
+// import {
+//   ConflictException,
+//   Injectable,
+//   NotFoundException,
+// } from "@nestjs/common";
+// import { Utilisateur } from "@prisma/client";
+// import { PrismaService } from "src/prisma/prisma.service";
+
+// @Injectable()
+// export class UserService {
+//   constructor(private prisma: PrismaService) {}
+
+//   async getUserIdFromUsername(username: string): Promise<number | null> {
+//     const user = await this.prisma.utilisateur.findFirst({
+//       where: {
+//         username,
+//       },
+//       select: {
+//         id: true,
+//       },
+//     });
+//     return user?.id || null;
+//   }
+
+//   async getUserInfo(username: string): Promise<Utilisateur> {
+//     const user = await this.prisma.utilisateur.findFirst({
+//       where: {
+//         username: username,
+//       },
+//     });
+
+//     if (!user) {
+//       throw new NotFoundException(`User with username ${username} not found`);
+//     }
+
+//     return user;
+//   }
+//   async getUserInfoPlus(id: number): Promise<Utilisateur> {
+//     const user = await this.prisma.utilisateur.findFirst({
+//       where: {
+//         id: id,
+//       },
+//       include: {
+//         friends: true,
+//         friendOf: true,
+//         ownedChatRooms: true,
+//         memberChatRooms: true,
+//         moderatorChatRooms: true,
+//         bannedChatRooms: true,
+//         matchHistoryWinner: true,
+//         matchHistoryLoser: true,
+//       },
+//     });
+//     if (!user) {
+//       throw new NotFoundException(`User with username ${id} not found`);
+//     }
+//     return user;
+//   }
+
+//   async getAllUsers(): Promise<Utilisateur[]> {
+//     return this.prisma.utilisateur.findMany();
+//   }
+
+//   //cette fonction est encore tr's myst/reiuse pour moi haha
+//   async getFriendsList(user: Utilisateur): Promise<Utilisateur[]> {
+//     const userData = await this.prisma.utilisateur.findUnique({
+//       where: { id: user.id },
+//       include: { friends: { include: { friend: true } } },
+//     });
+
+//     if (!userData) {
+//       throw new NotFoundException("User not found");
+//     }
+
+//     const friendsList = userData.friends.map((friendship) => friendship.friend);
+
+//     return friendsList;
+//   }
+
+//   async addFriend(user: Utilisateur, username: string) {
+//     const friend = await this.getUserInfo(username);
+//     if (!friend) {
+//       throw new NotFoundException("Friend not found");
+//     }
+
+//     const existingFriendship = await this.prisma.friendship.findFirst({
+//       where: {
+//         OR: [{ user: { id: user.id }, friend: { id: friend.id } }],
+//       },
+//     });
+//     if (existingFriendship) {
+//       throw new ConflictException("Friendship already exists");
+//     }
+//     async destroyFriend(userId: number, friendUserId: number): Promise<void> {
+//        friendUserId = Number(friendUserId)
+//       const friendship = await this.prisma.friendship.findFirst({
+//         where: {
+//           AND: [
+//             { userId: userId },
+//             { friendId: friendUserId },
+//           ],
+//         },
+//       });
+
+//       if (!friendship) {
+//         throw new NotFoundException('Friendship not found');
+//       }
+
+//       // Supprimer l'amitié
+//       await this.prisma.friendship.delete({
+//         where: {
+//           id: friendship.id,
+//         },
+//       });
+//     }
+//     // Créer l'enregistrement d'amitié
+//     console.log(user.username + " and " + username + " are now friends");
+//     const friendship = await this.prisma.friendship.create({
+//       data: {
+//         user: { connect: { id: user.id } },
+//         friend: { connect: { id: friend.id } },
+//       },
+//     });
+//   }
+//   async destroyFriend(userId: number, friendUserId: number): Promise<void> {
+//     const friendship = await this.prisma.friendship.findFirst({
+//       where: {
+//         AND: [{ userId: userId }, { friendId: friendUserId }],
+//       },
+//     });
+
+//     async updateAvatar(user : Utilisateur, image: any) {
+//         // const userToChange = await this.prisma.utilisateur.findUnique({
+//         //     where: {
+//         //     id: user.id,
+//         //     },
+//         // });
+//         // if (!userToChange) {
+//         //     throw new Error("Utilisateur non trouvé");
+//         // }
+
+//         const updatedUser= await this.prisma.utilisateur.update({
+//             where: {
+//                 id: user.id
+//             },
+//             data:{
+//                 avatar:image,
+//             }
+//         });
+//       return updatedUser
+//     }
+
+//     // Supprimer l'amitié
+//     await this.prisma.friendship.delete({
+//       where: {
+//         id: friendship.id,
+//       },
+//     });
+//   }
+
+//   async updateAvatar(user: Utilisateur, image: any) {
+//     // const userToChange = await this.prisma.utilisateur.findUnique({
+//     //     where: {
+//     //     id: user.id,
+//     //     },
+//     // });
+//     // if (!userToChange) {
+//     //     throw new Error("Utilisateur non trouvé");
+//     // }
+
+//     const updatedUser = await this.prisma.utilisateur.update({
+//       where: {
+//         id: user.id,
+//       },
+//       data: {
+//         avatar: image,
+//       },
+//     });
+//     return updatedUser;
+//   }
+
+//   async getUserMatchHistory(id: number) {
+//     id = Number(id);
+//     const user = await this.prisma.utilisateur.findUnique({
+//       where: { id },
+//       include: {
+//         matchHistoryWinner: {
+//           include: {
+//             winner: true,
+//             loser: true,
+//           },
+//         });
+
+//         const matchesWon = user.matchHistoryWinner.map(match => ({
+//           date: match.createdAt,
+//           winner: match.winner.username,
+//           loser: match.loser.username,
+//         }));
+
+//         const matchesLost = user.matchHistoryLoser.map(match => ({
+//           date: match.createdAt,
+//           winner: match.winner.username,
+//           loser: match.loser.username,
+//         }));
+//         console.log ("voici lhistorique", matchesLost, matchesWon)
+//         return {
+//           matchesWon,
+//           matchesLost,
+//         };
+//       }
+
+//       async  blockedUserIds(userId:number) {
+//           const id = Number(userId)
+//           const blockedUsers = await this.prisma.utilisateur.findUnique({
+
+//             where: { id: id },
+//             select: {
+//               blockedUsers: {
+//                 select: {
+//                   blockedUserId: true,
+//                 },
+//               },
+//             },
+//           });
+
+//           if (!blockedUsers) {
+//             return [];
+//           }
+//           else {
+//             const blockedUserIds = blockedUsers.blockedUsers.map((ban) => ban.blockedUserId);
+//             return blockedUserIds
+//           }
+//       }
+
+//       async checkIfUserExist(username: string): Promise<boolean> {
+//         const user = await this.prisma.utilisateur.findFirst({
+//           where: {
+//             username: username,
+//           },
+//         });
+
+//         return !!user;
+//       }
+// }
+
+// // await this.prisma.utilisateur.update({
+// //     where: { id: friend.id },
+// //     data: { friendOf: { connect: { id: friendship.id } } }
+// // });
+
 import {
   ConflictException,
   Injectable,
@@ -91,27 +341,10 @@ export class UserService {
     if (existingFriendship) {
       throw new ConflictException("Friendship already exists");
     }
-    async destroyFriend(userId: number, friendUserId: number): Promise<void> {
-       friendUserId = Number(friendUserId) 
-      const friendship = await this.prisma.friendship.findFirst({
-        where: {
-          AND: [
-            { userId: userId },
-            { friendId: friendUserId },
-          ],
-        },
-      });
-  
-      if (!friendship) {
-        throw new NotFoundException('Friendship not found');
-      }
-  
-      // Supprimer l'amitié
-      await this.prisma.friendship.delete({
-        where: {
-          id: friendship.id,
-        },
-      });
+    if (user.username === username) {
+      throw new ConflictException(
+        "You can't yourself as friend, sorry you are ALONE"
+      );
     }
     // Créer l'enregistrement d'amitié
     console.log(user.username + " and " + username + " are now friends");
@@ -123,33 +356,16 @@ export class UserService {
     });
   }
   async destroyFriend(userId: number, friendUserId: number): Promise<void> {
+    friendUserId = Number(friendUserId);
     const friendship = await this.prisma.friendship.findFirst({
       where: {
         AND: [{ userId: userId }, { friendId: friendUserId }],
       },
     });
 
-    async updateAvatar(user : Utilisateur, image: any) {
-        // const userToChange = await this.prisma.utilisateur.findUnique({
-        //     where: {
-        //     id: user.id, 
-        //     },
-        // });
-        // if (!userToChange) {
-        //     throw new Error("Utilisateur non trouvé"); 
-        // }
-      
-        const updatedUser= await this.prisma.utilisateur.update({
-            where: {
-                id: user.id
-            },
-            data:{
-                avatar:image,
-            }
-        });
-      return updatedUser 
+    if (!friendship) {
+      throw new NotFoundException("Friendship not found");
     }
-  
 
     // Supprimer l'amitié
     await this.prisma.friendship.delete({
@@ -190,63 +406,85 @@ export class UserService {
             winner: true,
             loser: true,
           },
-        });
-    
-        const matchesWon = user.matchHistoryWinner.map(match => ({
-          date: match.createdAt,
-          winner: match.winner.username,
-          loser: match.loser.username,
-        }));
-    
-        const matchesLost = user.matchHistoryLoser.map(match => ({
-          date: match.createdAt,
-          winner: match.winner.username,
-          loser: match.loser.username,
-        }));
-        console.log ("voici lhistorique", matchesLost, matchesWon)
-        return {
-          matchesWon,
-          matchesLost,
-        };
-      }
-      
-
-      async  blockedUserIds(userId:number) {
-          const id = Number(userId)
-          const blockedUsers = await this.prisma.utilisateur.findUnique({
-            
-            where: { id: id },
-            select: {
-              blockedUsers: {
-                select: {
-                  blockedUserId: true,
-                },
-              },
-            },
-          });
-      
-          if (!blockedUsers) {
-            return [];
-          }
-          else {
-            const blockedUserIds = blockedUsers.blockedUsers.map((ban) => ban.blockedUserId);
-            return blockedUserIds 
-          }
-      }
-    
-      async checkIfUserExist(username: string): Promise<boolean> {
-        const user = await this.prisma.utilisateur.findFirst({
-          where: {
-            username: username,
+        },
+        matchHistoryLoser: {
+          include: {
+            winner: true,
+            loser: true,
           },
-        });
-      
-        return !!user;
-      }
-}
-  
+        },
+      },
+    });
 
-// await this.prisma.utilisateur.update({
-//     where: { id: friend.id },
-//     data: { friendOf: { connect: { id: friendship.id } } }
-// });
+    const matchesWon = user.matchHistoryWinner.map((match) => ({
+      date: match.createdAt,
+      winner: match.winner.username,
+      loser: match.loser.username,
+    }));
+
+    const matchesLost = user.matchHistoryLoser.map((match) => ({
+      date: match.createdAt,
+      winner: match.winner.username,
+      loser: match.loser.username,
+    }));
+    console.log("voici lhistorique", matchesLost, matchesWon);
+    return {
+      matchesWon,
+      matchesLost,
+    };
+  }
+
+  async blockedUserIds(userId: number) {
+    const id = Number(userId);
+    const blockedUsers = await this.prisma.utilisateur.findUnique({
+      where: { id: id },
+      select: {
+        blockedUsers: {
+          select: {
+            blockedUserId: true,
+          },
+        },
+      },
+    });
+
+    if (!blockedUsers) {
+      return [];
+    } else {
+      const blockedUserIds = blockedUsers.blockedUsers.map(
+        (ban) => ban.blockedUserId
+      );
+      return blockedUserIds;
+    }
+  }
+
+  async checkIfUserExist(username: string): Promise<boolean> {
+    const user = await this.prisma.utilisateur.findFirst({
+      where: {
+        username: username,
+      },
+    });
+
+    return !!user;
+  }
+
+  async updateUsername(user: Utilisateur, username: string) {
+    // const userToChange = await this.prisma.utilisateur.findUnique({
+    //     where: {
+    //     id: user.id,
+    //     },
+    // });
+    // if (!userToChange) {
+    //     throw new Error("Utilisateur non trouvé");
+    // }
+
+    const updatedUser = await this.prisma.utilisateur.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        username: username,
+      },
+    });
+    return updatedUser;
+  }
+}
