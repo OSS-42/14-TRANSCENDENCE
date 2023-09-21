b8764a15e9ddcc1d6e1d57ea65a7ca151976c967
Cesar Menjivar <cemenjiv@student.42quebec.com>
2023-09-21 11:22:46 -0400

    Created 2FA validation page with input and submit button

--- frontend/src/pages/TwoFactor.tsx
+++ frontend/src/pages/TwoFactor.tsx
@@ -1,6 +1,37 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useState } from "react";
 
 export function TwoFactor() {
+  const [inputValue, setInputValue] = useState("");
+  const jwt_token = Cookies.get("jwt_token");
+
+  function handleChange(event: any){
+
+	const value = event.target.value;
+	console.log("The value is:", value);
+	setInputValue(value);
+  }
+
+  async function handle2FAClick() {
+    try {
+      const response = await axios.post(
+        `api/auth/verify2FA`,
+        {token: inputValue},
+        {
+          headers: {
+            Authorization: `Bearer ${jwt_token}`,
+          },
+        }
+      );
+      console.log("Response:", response.data);
+    } catch (error) {
+      console.error("Error verifying user 2FA", error);
+      throw error;
+    }
+  }
+
   return (
     <Box
       component="div"
@@ -14,7 +45,33 @@ export function TwoFactor() {
       }}
     >
       <strong>Welcome to the 2FA page!</strong> <br />
-      <br />Please enter your 6 digit-code that you can find on your authenticator application
+      <br />
+      Please enter your 6 digit-code that you can find on your authenticator
+      application to enter the site
+      <br />
+      <input
+        type="text"
+        id="2FA"
+        name="2FA"
+        required
+        minLength="6"
+        maxLength="6"
+        size="10"
+		onChange={handleChange}
+      />
+      <br />
+      <label htmlFor="2FA">
+        <Button
+          color="secondary"
+          variant="contained"
+          size="large"
+          component="span"
+          onClick={handle2FAClick}
+          sx={{ typography: { xs: "body2" } }}
+        >
+          Verify 2FA
+        </Button>
+      </label>
     </Box>
   );
 }
